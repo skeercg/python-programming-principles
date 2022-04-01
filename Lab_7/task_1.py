0e1bf03182b5087176c29f9011c760f421d72152
skeercg <39484137+skeercg@users.noreply.github.com>
2022-04-01 22:25:03 +0600

    1

--- Lab_7/task_1.py
+++ Lab_7/task_1.py
@@ -9,7 +9,6 @@ bg = pygame.image.load('bg.jpeg')
 bg = pygame.transform.scale(bg, (934, 700))
 leftHand = pygame.image.load('left.png')
 leftHand = pygame.transform.scale(leftHand, (80, 400))
-# leftHand = pygame.transform.flip(leftHand, flip_x=False, flip_y=True)
 rightHand = pygame.image.load('right.png')
 rightHand = pygame.transform.scale(rightHand, (80, 300))
 rotatedRightHand = rightHand
@@ -19,15 +18,6 @@ minutes = datetime.datetime.now().minute
 seconds = datetime.datetime.now().second
 rightHandAngle = minutes * 6
 leftHandAngle = seconds * 6
-
-def getOrigin(image, angle):
-    w, h = image.get_size()
-    box = [pygame.math.Vector2(p) for p in [(0, 0), (w, 0), (w, -h), (0, -h)]]
-    box_rotate = [p.rotate(angle) for p in box]
-    min_box = (min(box_rotate, key=lambda p: p[0])[0], min(box_rotate, key=lambda p: p[1])[1])
-    max_box = (max(box_rotate, key=lambda p: p[0])[0], max(box_rotate, key=lambda p: p[1])[1])
-    origin = (467 + min_box[0], 350 - max_box[1])
-    return origin
 
 while True:
     currentTime = datetime.datetime.now()
