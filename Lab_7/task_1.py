d3bd9137cff520c8632f63356986b145a3d0ecdf
skeercg <39484137+skeercg@users.noreply.github.com>
2022-04-01 22:53:11 +0600

    1

--- Lab_7/task_1.py
+++ Lab_7/task_1.py
@@ -23,9 +23,8 @@ while True:
     currentTime = datetime.datetime.now()
     screen.fill((0, 0, 0))
     screen.blit(bg, (0, 0))
-    screen.blit(rotatedLeftHand, rotatedLeftHand.get_rect(center = (467, 350)))
-
     screen.blit(rotatedRightHand, rotatedRightHand.get_rect(center = (467, 350)))
+    screen.blit(rotatedLeftHand, rotatedLeftHand.get_rect(center = (467, 350)))
 
     
 
@@ -34,13 +33,19 @@ while True:
             pygame.quit()
     
     rotatedRightHand = pygame.transform.rotate(rightHand, -rightHandAngle)
-    rightHandAngle += abs(currentTime.minute - minutes) * 6
+    if (currentTime.minute == 0):
+        rightHandAngle += abs(60 - minutes) * 6
+    else:
+        rightHandAngle += abs(currentTime.minute - minutes) * 6
 
     minutes = currentTime.minute
 
     rotatedLeftHand = pygame.transform.rotate(leftHand, -leftHandAngle)
-    leftHandAngle += abs(currentTime.second - seconds) * 6
-
+    if (currentTime.second == 0):
+        leftHandAngle += abs(60 - seconds) * 6
+    else:
+        leftHandAngle += abs(currentTime.second - seconds) * 6
+    
     seconds = currentTime.second
 
     pygame.display.flip()
