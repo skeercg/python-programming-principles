bd95aa43964c55f9e570a943404e6442746ccb7c
skeercg <39484137+skeercg@users.noreply.github.com>
2022-05-02 16:40:42 +0600

    1

--- Lab_10/phonebook.py
+++ Lab_10/phonebook.py
@@ -1,3 +1,56 @@
 import psycopg2
 
-conn = psycopg2.connect()
+try: 
+    connection = psycopg2.connect(
+        dbname = 'phonebook', 
+        user = 'khas', 
+        password = '', 
+        host = 'localhost'
+    )
+
+    connection.autocommit = True
+
+    cursor = connection.cursor()
+
+    # cursor.execute(
+    #     """CREATE TABLE phonebook(
+    #         id serial PRIMARY KEY,
+    #         name varchar(50) NOT NULL,
+    #         phone varchar(50) NOT NULL
+    #     );"""
+    # )
+
+    # cursor.execute(
+    #     """INSERT INTO phonebook (name, phone)
+    #     VALUES ('Ahmad', '77015554422');"""
+    #     )
+
+    # cursor.execute(
+    #     """UPDATE phonebook
+    #     SET name = 'Alikhan', phone = '88005553535'
+    #     WHERE id = 1;"""
+    #     )
+
+    # cursor.execute(
+    #     """COPY phonebook(id, name, phone) 
+    #     FROM '/Users/khas/Documents/codes/pp2/Lab_10/phones.csv' 
+    #     WITH (FORMAT CSV, DELIMITER ',', HEADER)"""
+    #     )
+
+    # cursor.execute(
+    #     """DELETE FROM phonebook
+    #     WHERE name = 'Alikhan';"""
+    #     )
+
+    cursor.execute("""SELECT * FROM phonebook;""")
+
+    print(cursor.fetchall())
+
+except Exception as exception:
+    print("Error occured", exception)
+finally:
+    if connection:
+        cursor.close()
+        connection.close()
+
+
